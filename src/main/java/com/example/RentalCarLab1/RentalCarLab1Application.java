47413bf38cb53caa01f7815ac131b997aef4089c
MaelysCC <maelys.de-crouy-chanel@efrei.net>
2026-08-26 15:42:37 +0200

    Initial commit

---
+++ src/main/java/com/example/RentalCarLab1/RentalCarLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.RentalCarLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarLab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RentalCarLab1/RentalCarLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RentalCarLab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
